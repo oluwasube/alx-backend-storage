a024438c398558c106843d6c76ca89be6f3da303
oluwasube <enibeke20@gmail.com>
2023-01-14 18:22:23 +0000

    task 1 completed

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+-- script that creates a table users with follwing fields
+-- id, email, name
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
+
